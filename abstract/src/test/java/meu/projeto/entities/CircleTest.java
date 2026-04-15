3b34953dddba19139448c028d8d6dfec956be964
alineferreiraqa <41009515+alinefds@users.noreply.github.com>
2026-04-14 23:20:55 -0300

    testCircle

--- abstract/src/test/java/meu/projeto/entities/CircleTest.java
+++ abstract/src/test/java/meu/projeto/entities/CircleTest.java
@@ -1,19 +1,33 @@
 package meu.projeto.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import meu.projeto.entities.enums.Color;
 
 public class CircleTest {
 
-     @Test
-    public void deveCalcularAreaUsandoPolimorfismo() {
-        Shape s = new Circle(3.0, Color.RED);
+    private Circle c = new Circle(3.0, Color.RED);
+    private Shape s = new Circle(3.0, Color.RED);
 
+    @Test
+    public void deveCalcularAreaCorretamente() {
+        assertEquals(28.274333882308138, c.area());
+    }
+
+    @Test
+    public void deveCalcularAreaUsandoPolimorfismo() {
         assertEquals(28.274333882308138, s.area());
     }
 
+    @Test
+    public void deveExibirToString() {
 
+        String result = c.toString();
+
+        assertTrue(result.contains("Circle"));
+    }
 
 }
--- abstract/src/test/java/meu/projeto/entities/ShapeTest.java
+++ abstract/src/test/java/meu/projeto/entities/ShapeTest.java
@@ -2,4 +2,6 @@ package meu.projeto.entities;
 
 public class ShapeTest {
 
+
+
 }
